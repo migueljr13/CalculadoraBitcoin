3ac306fdaa25157676fdf6b6ab1dad6a20d2a666
miguejr13 <miguelpta@gmail.com>
2021-04-11 19:53:28 -0300

    obtendo outras informações da criptomoeda escolhida.

--- app/src/main/java/com/courses/calculadorabitcoin/MainActivity.kt
+++ app/src/main/java/com/courses/calculadorabitcoin/MainActivity.kt
@@ -1,27 +1,42 @@
 package com.courses.calculadorabitcoin
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import com.courses.calculadorabitcoin.adapter.CriptomoedaAdapter
+import com.courses.calculadorabitcoin.model.pojo.CriptomoedaItem
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import org.json.JSONObject
 import java.net.URL
+import java.text.DecimalFormat
 import java.text.NumberFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var cotacaoBitcoin: Double = 0.0
+    private var volummeCriptomoeda: Double = 0.0
+    private var minimaCriptomoeda: Double = 0.0
+    private var maximaCriptomoeda: Double = 0.0
+    private var cotacaoCriptomoeda: Double = 0.0
+
     private lateinit var txt_cotacao: TextView
+    private lateinit var txt_ultimo: TextView
+    private lateinit var txt_maxima: TextView
+    private lateinit var txt_minima: TextView
+    private lateinit var txt_volume: TextView
+
     private lateinit var txt_qtd_bitcoin: TextView
     private lateinit var txt_valor: TextView
     private lateinit var btn_calcular: Button
     private lateinit var btn_limpar: Button
+    private lateinit var spnCriptomoeda: Spinner
+
+    private lateinit var adapter: CriptomoedaAdapter
 
     companion object {
-        private const val BASE_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
+        private const val BASE_URL = "https://www.mercadobitcoin.net/api/"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +44,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         txt_cotacao = findViewById(R.id.txt_cotacao)
+        txt_ultimo = findViewById(R.id.txt_ultimo)
+        txt_maxima = findViewById(R.id.txt_maxima)
+        txt_minima = findViewById(R.id.txt_minima)
+        txt_volume = findViewById(R.id.txt_volume)
+
         txt_qtd_bitcoin = findViewById(R.id.txt_qtd_bitcoins)
         txt_valor = findViewById(R.id.txt_valor)
         btn_calcular = findViewById(R.id.btn_calcular)
         btn_limpar = findViewById(R.id.btn_limpar)
+        spnCriptomoeda = findViewById(R.id.spnCriptomoeda)
 
-        buscaCotacao()
+        preencheSpinner()
+        spnCriptomoeda.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                positon: Int,
+                id: Long
+            ) {
+                buscaCotacao("${BASE_URL}${getCoinSelecionado(positon)}/ticker/")
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "Nenhuma criptomoeda escolhida.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         btn_calcular.setOnClickListener {
             calcular()
@@ -43,6 +82,28 @@ class MainActivity : AppCompatActivity() {
             txt_valor.text = ""
             txt_qtd_bitcoin.text = formataQuantidadeBitcoin(0.0)
         }
+    }
+
+    /**
+     * Retorna a criptomoeda escolhida no spinner.
+     *
+     * @author Miguel
+     * @param positon
+     */
+    private fun getCoinSelecionado(positon: Int) = when (positon) {
+        0 -> "BTC"
+        1 -> "ETH"
+        else -> "Nenhum"
+    }
+
+    private fun preencheSpinner() {
+
+        val listaCriptomoeda = arrayListOf(
+            CriptomoedaItem("Bitcoin (BTC)", R.mipmap.ic_btc),
+            CriptomoedaItem("Ethereum (ETH)", R.mipmap.ic_eth)
+        )
+        adapter = CriptomoedaAdapter(this, listaCriptomoeda)
+        spnCriptomoeda.adapter = adapter
     }
 
     private fun calcular() {
@@ -54,22 +115,46 @@ class MainActivity : AppCompatActivity() {
 
         val valor_digitado = txt_valor.text.toString().formataDouble()
 
-        val resultado = if (cotacaoBitcoin > 0) valor_digitado / cotacaoBitcoin else 0.0
+        val resultado = if (cotacaoCriptomoeda > 0) valor_digitado / cotacaoCriptomoeda else 0.0
         txt_qtd_bitcoin.text = formataQuantidadeBitcoin(resultado)
     }
 
-    private fun buscaCotacao() {
+    private fun buscaCotacao(url: String) {
 
         doAsync {
-            val resposta = URL(BASE_URL).readText()
-            cotacaoBitcoin = JSONObject(resposta).getJSONObject("ticker").getDouble("last")
-            val formatacao = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
-            val cotacaoFormatada = formatacao.format(cotacaoBitcoin)
+            val resposta = URL(url).readText()
+            cotacaoCriptomoeda = JSONObject(resposta).getJSONObject("ticker").getDouble("last")
+            maximaCriptomoeda = JSONObject(resposta).getJSONObject("ticker").getDouble("high")
+            minimaCriptomoeda = JSONObject(resposta).getJSONObject("ticker").getDouble("low")
+            volummeCriptomoeda = JSONObject(resposta).getJSONObject("ticker").getDouble("vol")
+
             uiThread {
-                txt_cotacao.setText(cotacaoFormatada)
+                txt_cotacao.setText(formataMoeda(cotacaoCriptomoeda))
+                txt_ultimo.setText(formataMoeda(cotacaoCriptomoeda))
+                txt_maxima.setText(formataMoeda(maximaCriptomoeda))
+                txt_minima.setText(formataMoeda(minimaCriptomoeda))
+                txt_volume.setText(formataVolume(volummeCriptomoeda))
             }
         }
     }
+}
+
+private fun formataVolume(valor : Double) : String {
+    val formatacao = DecimalFormat("##.##")
+    return formatacao.format(valor).replace('.', ',')
+}
+
+/**
+ * Formata a moeda brasileira (R$) de um valor do tipo Double
+ *
+ * @author Miguel
+ * @param valor
+ **/
+private fun formataMoeda(valor: Double): String {
+
+    val formatacao = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+    val cotacaoFormatada = formatacao.format(valor)
+    return cotacaoFormatada
 }
 
 /**
@@ -80,12 +165,12 @@ class MainActivity : AppCompatActivity() {
  * @return String
  *
  **/
-fun formataQuantidadeBitcoin(valor: Double) = "%8f".format(valor)
+private fun formataQuantidadeBitcoin(valor: Double) = "%8f".format(valor)
 
 /**
-* Formata um valor monetário do tipo de dado String para o tipo de dado Double
+ * Formata um valor monetário do tipo de dado String para o tipo de dado Double
  *
  * @author Miguel
  * @return Double
-* */
-fun String.formataDouble() = this.replace(",", ".").toDouble()
+ * */
+private fun String.formataDouble() = this.replace(",", ".").toDouble()
